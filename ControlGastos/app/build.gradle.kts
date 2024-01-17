9a922e7b328b825317e40b697e7c8b87c103138b
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2024-01-17 10:57:00 +0100

    Implementacion Kotlin

--- ControlGastos/app/build.gradle.kts
+++ ControlGastos/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
