6c96e4e6123fcf288651606396e5312795456d10
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2024-01-21 22:32:17 +0100

    Menu quitado

--- EjAndroid01/app/src/main/java/com/example/ejandroid01/MainActivityND.kt
+++ EjAndroid01/app/src/main/java/com/example/ejandroid01/MainActivityND.kt
@@ -60,11 +60,11 @@ private lateinit var binding: ActivityMainNdBinding
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    //override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_activity_n_d, menu)
-        return true
-    }
+      //  menuInflater.inflate(R.menu.main_activity_n_d, menu)
+        //return true
+    //}
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main_activity_nd)
